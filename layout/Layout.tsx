5d82df84c77c55f395874b94cfb8b4a14a8dbb55
asmirnov <a.smirnov@zebrains.team>
2021-07-02 16:29:18 +0300

    fix imports

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
-import cn from 'classnames';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
